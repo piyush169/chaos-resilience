980828dc77bad9b4fe0e8cb13dcaf3bee893c882
Piyush Singh <piyu1219@gmail.com>
2026-02-20 01:29:25 +0530

    updated main.tf(added vpc)

--- infra/main.tf
+++ infra/main.tf
@@ -16,6 +16,7 @@ resource "aws_subnet" "chaos_subnet" {
   vpc_id                  = aws_vpc.chaos_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true 
+  availability_zone       = "us-east-1a"
   tags = { Name = "chaos-subnet" }
 }
 
